e759f87f6a75f8007db989210b0d47903b5c955c
Al amin <alaminmridha2004@gmail.com>
2024-11-28 15:08:33 +0600

    Add Button Shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,22 @@ export default {
       center: true,
     },
     extend: {
+
+      // Add Custom Font Family
       fontFamily: {
         "montserrat" : ["Montserrat"],
         "workSans" : ["Work Sans"],
       },
+
+      // Add Required Color Theme 
       colors: {
         themeColor: "#002228",
         primary: "#0FF1F6",
+      },
+
+      // Add Button Shadow 
+      boxShadow:{
+        "btnShadow": "0 0 45px rgba(15, 241, 246, 0.25)",
       },
     },
   },
