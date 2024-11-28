c2db1d1b0fd06ecc2685530e1ce9c4823591bf9e
Al amin <alaminmridha2004@gmail.com>
2024-11-28 15:44:46 +0600

    Some Update (Slider text line Height)

--- src/components/common/sliderCard.jsx
+++ src/components/common/sliderCard.jsx
@@ -4,7 +4,7 @@ function SliderCard({ desc, name, tag }) {
   return (
     <div className="max-w-[920px] mx-auto px-3 py-1 text-center font-workSans">
       <VscQuote className="mx-auto text-7xl p-4 bg-primary/25 text-primary rounded-full scale-80 md:scale-100" />
-      <p className="my-5 md:my-11 text-2xl md:text-[32px] font-light text-white">
+      <p className="my-5 md:my-11 text-2xl md:text-[32px] font-light text-white md:leading-[40px]">
         {desc}
       </p>
       <h2 className="text-xl md:text-2xl text-primary font-semibold">{name}</h2>
